34ad5bed9f5628bb15c7bf46873fa0c567acaa75
Yuya.komai <riiiiikooooo5@gmail.com>
2020-03-29 11:45:49 +0900

    カメラ設定まで完了

--- Instagram/SettingViewController.swift
+++ Instagram/SettingViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class SettingViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBAction func handleChangeUserName(_ sender: Any) {
+        if let displayName = userNameTextField.text {
+            
+            if displayName.isEmpty {
+                SVProgressHUD.showError(withStatus: "ユーザー名を入力して下さい")
+                return
+            }
+            
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = displayName
+                changeRequest.commitChanges { (error) in
+                    if let error = error {
+                        SVProgressHUD.showError(withStatus: "ユーザー名の変更に失敗しました。")
+                        print("DEBUG:ユーザー名変更失敗" + error.localizedDescription)
+                    }
+                    print("DEBUG: ユーザー名変更完了")
+                    
+                    SVProgressHUD.showSuccess(withStatus: "ユーザー名を変更しました！")
+                }
+            }
+        }
+        self.view.endEditing(true)
+        
+    }
+    
+    @IBAction func handleLogoutButton(_ sender: Any) {
+        try! Auth.auth().signOut()
+        
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        self.present(loginViewController!, animated: true, completion: nil)
+        
+        tabBarController?.selectedIndex = 0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let user = Auth.auth().currentUser
+        if let user = user {
+            userNameTextField.text = user.displayName
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
